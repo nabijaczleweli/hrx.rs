775b4d4db16fc7902d76fc11772f480b4e4179a0
nabijaczleweli <nabijaczleweli@gmail.com>
2019-02-09 20:56:19 +0100

    Add path parsing w/tests

---
+++ tests/grammar/invalid_paths.rs
@@ -0,0 +1,146 @@
+//! This is an adaptation of the example/invalid/invalid-paths.hrx file in the original google/hrx repository.
+
+
+use hrx::grammar::{ParseError, path};
+
+
+#[test]
+fn initial_slash() {
+    assert_eq!(path("/file"),
+               Err(ParseError {
+                   line: 1,
+                   column: 1,
+                   offset: 0,
+                   expected: vec!["Any character other than U+0000 through U+001F, U+007F DELETE, U+002F SOLIDUS, U+003A COLON, or U+005C REVERSE SOLIDUS"]
+                       .into_iter()
+                       .collect(),
+               }));
+}
+
+#[test]
+fn double_slash() {
+    assert_eq!(path("dir//file"),
+               Err(ParseError {
+                   line: 1,
+                   column: 5,
+                   offset: 4,
+                   expected: vec!["Any character other than U+0000 through U+001F, U+007F DELETE, U+002F SOLIDUS, U+003A COLON, or U+005C REVERSE SOLIDUS"]
+                       .into_iter()
+                       .collect(),
+               }));
+}
+
+#[test]
+fn final_slash() {
+    assert_eq!(path("dir//"),
+               Err(ParseError {
+                   line: 1,
+                   column: 5,
+                   offset: 4,
+                   expected: vec!["Any character other than U+0000 through U+001F, U+007F DELETE, U+002F SOLIDUS, U+003A COLON, or U+005C REVERSE SOLIDUS"]
+                       .into_iter()
+                       .collect(),
+               }));
+}
+
+#[test]
+fn single_dot() {
+    assert_eq!(path("."),
+               Err(ParseError {
+                   line: 1,
+                   column: 2,
+                   offset: 1,
+                   expected: vec!["Invalid '.' or '..' path component",
+                                  "Any character other than U+0000 through U+001F, U+007F DELETE, U+002F SOLIDUS, U+003A COLON, or U+005C REVERSE SOLIDUS"]
+                       .into_iter()
+                       .collect(),
+               }));
+}
+
+#[test]
+fn double_dot() {
+    assert_eq!(path(".."),
+               Err(ParseError {
+                   line: 1,
+                   column: 3,
+                   offset: 2,
+                   expected: vec!["Invalid '.' or '..' path component",
+                                  "Any character other than U+0000 through U+001F, U+007F DELETE, U+002F SOLIDUS, U+003A COLON, or U+005C REVERSE SOLIDUS"]
+                       .into_iter()
+                       .collect(),
+               }));
+}
+
+#[test]
+fn single_dot_component() {
+    assert_eq!(path("dir/./file"),
+               Err(ParseError {
+                   line: 1,
+                   column: 6,
+                   offset: 5,
+                   expected: vec!["Invalid '.' or '..' path component",
+                                  "Any character other than U+0000 through U+001F, U+007F DELETE, U+002F SOLIDUS, U+003A COLON, or U+005C REVERSE SOLIDUS"]
+                       .into_iter()
+                       .collect(),
+               }));
+}
+
+#[test]
+fn double_dot_component() {
+    assert_eq!(path("dir/../file"),
+               Err(ParseError {
+                   line: 1,
+                   column: 7,
+                   offset: 6,
+                   expected: vec!["Invalid '.' or '..' path component",
+                                  "Any character other than U+0000 through U+001F, U+007F DELETE, U+002F SOLIDUS, U+003A COLON, or U+005C REVERSE SOLIDUS"]
+                       .into_iter()
+                       .collect(),
+               }));
+}
+
+#[test]
+fn backslash() {
+    assert_eq!(path("dir\\file"),
+               Err(ParseError {
+                   line: 1,
+                   column: 4,
+                   offset: 3,
+                   expected: vec!["/",
+                                  "Any character other than U+0000 through U+001F, U+007F DELETE, U+002F SOLIDUS, U+003A COLON, or U+005C REVERSE SOLIDUS"]
+                       .into_iter()
+                       .collect(),
+               }));
+}
+
+#[test]
+fn invalid_ascii() {
+    assert_eq!(path("\x7F"),
+               Err(ParseError {
+                   line: 1,
+                   column: 1,
+                   offset: 0,
+                   expected: vec!["Any character other than U+0000 through U+001F, U+007F DELETE, U+002F SOLIDUS, U+003A COLON, or U+005C REVERSE SOLIDUS"]
+                       .into_iter()
+                       .collect(),
+               }));
+}
+
+#[test]
+fn colon() {
+    assert_eq!(path("C:/file"),
+               Err(ParseError {
+                   line: 1,
+                   column: 2,
+                   offset: 1,
+                   expected: vec!["/",
+                                  "Any character other than U+0000 through U+001F, U+007F DELETE, U+002F SOLIDUS, U+003A COLON, or U+005C REVERSE SOLIDUS"]
+                       .into_iter()
+                       .collect(),
+               }));
+}
+
+// #[test]
+// fn no_space_before_path() {
+//     <======>file
+// }
---
+++ tests/lib.rs
@@ -0,0 +1,4 @@
+extern crate hrx;
+
+
+mod grammar;
