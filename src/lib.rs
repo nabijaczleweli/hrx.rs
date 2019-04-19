5b2f82c06e98eebcbf6e40c42a378e19c28ef8ed
nabijaczleweli <nabijaczleweli@gmail.com>
2019-04-19 07:42:21 +0200

    Add toplevel doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,77 @@
+//! A Rust implementation of the HRX plain text archive format.
+//!
+//! Consult the [`google/hrx`](//github.com/google/hrx) repo for the details on the format.
+//!
+//! # Examples
+//!
+//! ```
+//! # use hrx::{HrxEntryData, HrxArchive, HrxEntry, HrxError, HrxPath};
+//! # use std::str::FromStr;
+//! let input_text = "<===> input.scss
+//! ul {
+//!   li {
+//!     list-style: none;
+//!   }
+//! }
+//!
+//! <===>
+//! Generated files
+//! <===> out/
+//!
+//! <===> out/input.css
+//! ul li {
+//!   list-style: none;
+//! }
+//! ";
+//!
+//! let mut archive = HrxArchive::from_str(input_text)?;
+//!
+//! assert_eq!(archive.comment, None);
+//! assert_eq!(archive.entries,
+//!            [(HrxPath::from_str("input.scss")?,
+//!              HrxEntry {
+//!                 comment: None,
+//!                 data: HrxEntryData::File {
+//!                     body: Some("ul {\n  li {\n    list-style: none;\n  }\n}\n".to_string()),
+//!                 },
+//!              }),
+//!             (HrxPath::from_str("out")?,
+//!              HrxEntry {
+//!                 comment: Some("Generated files".to_string()),
+//!                 data: HrxEntryData::Directory,
+//!              }),
+//!             (HrxPath::from_str("out/input.css")?,
+//!              HrxEntry {
+//!                 comment: None,
+//!                 data: HrxEntryData::File {
+//!                     body: Some("ul li {\n  list-style: none;\n}\n".to_string()),
+//!                 },
+//!              })].iter().cloned().collect());
+//!
+//! archive.entries.remove(&HrxPath::from_str("out")?);
+//! archive.comment = Some("Snapshot of commit 264a050c".to_string());
+//!
+//! let mut out = vec![];
+//! archive.serialise(&mut out).unwrap();
+//!
+//! assert_eq!(String::from_utf8(out).unwrap(), "<===> input.scss
+//! ul {
+//!   li {
+//!     list-style: none;
+//!   }
+//! }
+//!
+//! <===> out/input.css
+//! ul li {
+//!   list-style: none;
+//! }
+//!
+//! <===>
+//! Snapshot of commit 264a050c");
+//! # Ok::<(), HrxError>(())
+//! ```
+
+
 extern crate linked_hash_map;
 extern crate lazysort;
 #[macro_use]
